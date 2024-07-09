78de96835e53f773494d435f599e2e9c688047a8
Andy Lindsay <andydlindsay@gmail.com>
2024-07-09 14:03:13 -0700

    breakout over

--- breakout-react-router/demo/src/App.js
+++ breakout-react-router/demo/src/App.js
@@ -1,15 +1,30 @@
 import './App.css';
 
-// import Home from './components/Home';
-// import About from './components/About';
-// import Products from './components/Products';
-// import Product from './components/Product';
-// import Nav from './components/Nav';
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+
+import Home from './components/Home';
+import About from './components/About';
+import Products from './components/Products';
+import Product from './components/Product';
+import Nav from './components/Nav';
 
 const App = () => {
   return (
     <div className="App">
       <h2>React Router Demo</h2>
+
+      <BrowserRouter>
+        <Nav />
+
+        <Routes>
+          <Route path="*" element={<h2>Four oh Four!</h2>} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/admin" element={<h2>Custom Admin Page</h2>} />
+          <Route path="/products/:productId" element={<Product />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
--- breakout-react-router/demo/src/components/Product.jsx
+++ breakout-react-router/demo/src/components/Product.jsx
@@ -1,7 +1,26 @@
+import { useEffect } from 'react';
+import {useParams, useNavigate} from 'react-router-dom';
+
 const Product = () => {
+  const params = useParams(); // req.params.productId params.productId
+  console.log(params);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/about'); // res.redirect('/about');
+    }, 3000);
+  }, []);
+
+  // useEffect(() => {
+  //   fetch(`/api/products/${params.productId}`)
+  //     .then()
+  // }, [params.productId])
+
   return (
     <div>
-      <h2>Product Component</h2>
+      <h2>Product Component for #{params.productId}</h2>
     </div>
   );
 };
